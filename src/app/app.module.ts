53b8e8df7e34e30e53f81b3eaeca3de848ec68c2
Rajeev Patil <rajeevvpatil899@gmail.com>
2021-08-14 20:42:41 -0700

    courses and contact us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
